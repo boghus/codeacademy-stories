f633d4e393a901e208be21be632e3b9c13212454
Carlos Berdejo <cberdejo@buk.mx>
2024-07-04 01:34:20 -0400

    feat: Crear pagina

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/home', to: 'pages#home'
+  get '/thanks', to: 'pages#thanks'
+
+
+
+  resources :signups
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
